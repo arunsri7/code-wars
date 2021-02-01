e29931dcfff8092077ae4824cdb9f880cc70080e
arunsri7 <arun.i1997@gmail.com>
2021-02-01 18:25:13 +0530

    [8kyu][golang] Do I get a bonus?

---
+++ 8kyu/golang/doIGetABonus.go
@@ -0,0 +1,12 @@
+package kata
+
+import (
+	"fmt"
+)
+
+func BonusTime(salary int, bonus bool) string {
+	if bonus {
+		return fmt.Sprintf("£%d", salary*10)
+	}
+	return fmt.Sprintf("£%d", salary)
+}
