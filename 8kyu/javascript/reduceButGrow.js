54b64991c79904993f71b45b27de20289c62616b
arunsri7 <arun.i1997@gmail.com>
2021-02-20 22:21:58 +0530

    [8kyu] reduceButGrow.js

--- 8kyu/javascript/reduceButGrow.js
+++ 8kyu/javascript/reduceButGrow.js
@@ -1,9 +1,13 @@
-function grow(x){
-  
-    let product = 1
-    for(i=0;i<x.length;i++){
-      product = product * x[i]
-    }
-    
-    return product
+function grow(x) {
+  let product = 1;
+  for (i = 0; i < x.length; i++) {
+    product = product * x[i];
   }
+
+  return product;
+}
+
+//using reduce
+function grow(x) {
+  return x.reduce((a, b) => a * b);
+}
